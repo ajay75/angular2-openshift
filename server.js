818cf51262ce40a39499f043323fa97d12ce65b0
Jerome Cristante <jerome.cristante@its4u.lu>
2016-06-16 10:43:46 +0200

    refactoring

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
+var restify = require('restify'),
+    fs      = require('fs');
+	
+var app = restify.createServer();
+app.use(restify.queryParser());
+app.use(restify.CORS());
+app.use(restify.fullResponse());
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+	
+app.get('/', function (req, res, next) {
+  var data = fs.readFileSync(__dirname + '/index.html');
+  res.status(200);
+  res.header('Content-Type', 'text/html');
+  res.end(data.toString().replace(/host:port/g, req.header('Host')));
+});
 
-app.get('/', function (req, res) {
-    res.render('../index.html');
+app.listen(port, ip, function () {
+	console.log('Server running on http://%s:%s', ip, port);
 });
 
 // error handling
@@ -21,8 +28,3 @@ app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
